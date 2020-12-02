7600e9dd29f0c2ffe1545fdb821faffd8720b524
leehaeina <leehaein0164@gmail.com>
2020-12-02 11:13:27 +0900

    like rerendering 적용

--- js/details.js
+++ js/details.js
@@ -3,21 +3,59 @@ var data = temp[1].split("=");
 const contentId = data[1];
 console.log("data[1]",data[1])
 const serviceKey = "Q1rnD0P2lMbHzUSEqclxucsYKUIwcWXh%2BV48SXPuLPs7%2FxbrkIMg%2BN2HV9ELlnWxyawWxv4xQuo4BkUsFgs%2FYg%3D%3D";
+function likeSelecte (){
+    console.log("hi")
+    $.ajax({
+        type: "get",
+        url: 'http://localhost:7000/admin/like/list',
+         datatype: 'json',
+         async:false,
+         contentType: "application/json; charset=utf-8",
+       
+       success: function(res){
+        
+         let data = res.map(v=>v.contentId)
+        
+           if(document.getElementById('like-button')){
+        if(data.includes(Number(contentId))){console.log(data.includes(contentId));document.getElementById('like-button').classList.toggle('selected'); }
+        
+         }
+        }
+        })  
+        return;
 
-function detailCommon() {
+}
+function getItem(requestUrl){
+    let data;
+    $.ajax({
+        type: "get",
+        url: requestUrl,
+        datatype: 'json',
+        async:false,
+        success: function (msg) {
+            data= msg.response.body.items.item;
+            console.log(data)
+           
+        }
+       
+    })
+    console.log(data)
+    return data;
+}
+async function detailCommon() {
     var requestUrl = '';
     requestUrl += "http://api.visitkorea.or.kr/openapi/service/rest/KorService/detailCommon?ServiceKey=";
     requestUrl += serviceKey;
     requestUrl += "&contentId=" + contentId;
     requestUrl += "&MobileOS=ETC&MobileApp=TourAPI3.0_Guide&defaultYN=Y&firstImageYN=Y&areacodeYN=Y&catcodeYN=Y&addrinfoYN=Y&mapinfoYN=Y&overviewYN=Y&transGuideYN=Y&_type=json";
     console.log(requestUrl)
-    $.ajax({
-        type: "get",
-        url: requestUrl,
-        datatype: 'json',
-        success: function (msg) {
-            var item = msg.response.body.items.item;
-            var common = '';
+    let likeSelected = false;
+    
+    var common = '';
+    let item = getItem(requestUrl)
+   
+    
+    if(item){
             common += '<div class="details-title">';
             common += '<h2>' + item.title + '</h2></div>';
             common += '<div class="details-main">';
@@ -26,14 +64,18 @@ function detailCommon() {
             common += '<li><strong>홈페이지</strong> : ' + item.homepage + '</li>';
             common += '<li><strong>주소</strong> : ' + item.addr1 + '</li></ul>';
             // 좋아요 버튼
-            common += '<button type="button" id="like-button"><svg class="heart-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><path d="M91.6 13A28.7 28.7 0 0 0 51 13l-1 1-1-1A28.7 28.7 0 0 0 8.4 53.8l1 1L50 95.3l40.5-40.6 1-1a28.6 28.6 0 0 0 0-40.6z"/></svg>Like</button></div></div>';
+            common += '<button type="button" id="like-button" ><svg class="heart-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><path d="M91.6 13A28.7 28.7 0 0 0 51 13l-1 1-1-1A28.7 28.7 0 0 0 8.4 53.8l1 1L50 95.3l40.5-40.6 1-1a28.6 28.6 0 0 0 0-40.6z"/></svg>Like</button></div></div>';
             common += '<div class="details-info1"><h3>소개</h3>';
             common += '<p>' + item.overview + '</p></div>'
             common +=
                 $(".details").append(common);
             detailInfo(item.contenttypeid);
-        }
-    })
+    }
+    likeSelecte();
+ 
+
+
+return common;
 }
 
 function detailInfo(typeid) {
@@ -109,7 +151,7 @@ function likeBtn() {
     const likeBtn = document.getElementById('like-button');
     likeBtn.addEventListener('click', () => {
         likeBtn.classList.toggle('selected');
-     
+    
         let onSelect =likeBtn.className
         if( onSelect==='selected'){
             $.ajax({
@@ -122,8 +164,18 @@ function likeBtn() {
        
                  console.log(data)
                  }
-                }) 
+                })  
         }
-        else  console.log("notselected")   //post 기능 추가 
+        else { $.ajax({
+            type: "delete",
+            url: 'http://localhost:7000/admin/like',
+             datatype: 'json',
+             contentType: "application/json; charset=utf-8",
+            data:JSON.stringify({contentId:contentId}),
+           success: function(data){
+   
+             console.log(data)
+             }
+            })  }  //post 기능 추가 
     })
 }
